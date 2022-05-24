3ace0cd08206fc2e483ecda13a0e6d086d1b308d
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-05-24 23:36:04 +0100

    additional css file addedd

--- src/script.js
+++ src/script.js
@@ -27,14 +27,6 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-/*
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}*/
-
 // ANIMATED TYPEWRITER
 
 // List of sentences
