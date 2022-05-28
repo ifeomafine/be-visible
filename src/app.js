ce4593da945e29609e31a1431eb661534e921d59
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-05-29 00:25:53 +0100

    api added

---
+++ src/app.js
@@ -0,0 +1,47 @@
+
+// FOR THE MODAL FORM
+// Get the modal
+ const modalBox = document.getElementById('recruitModal')
+ // Get the button that opens the modal
+ const button = document.getElementById('theirBtn')
+ // Get the <spanIcon> element that closes the modal
+ const spanIcon = document.querySelector('.close-icon')
+
+ // When the user clicks the button, open the modal 
+ button.addEventListener('click' , () => {
+     modalBox.style.display = "block"
+ })
+
+ // When the user clicks on <spanIcon> (x), close the modal
+ spanIcon.addEventListener('click' , () => {
+     modalBox.style.display = "none"
+ })
+ 
+// TO TOGGLE PASSWORD VISIBILITY
+let Show = document.getElementById('checkBox');
+let password = document.getElementById('passy')
+
+Show.addEventListener('click' , () => {
+	let type = password.getAttribute('type') === 
+	'password' ? 'text' : 'password';
+	password.setAttribute('type' , type);
+})
+// FOR API INTEGRATION
+const quote = document.querySelector('.quotes')
+const author = document.querySelector('.display')
+
+  setInterval(() => {
+	fetch("https://type.fit/api/quotes")
+	.then(response => response.json())
+	.then(data => {
+		const randomTxt = Math.floor(Math.random() * data.length);
+		const item = data[randomTxt]
+		quote.innerHTML = `${item.text}`
+		author.innerHTML = `${item.author}`
+  
+		if (item.author === " " || item.author === null) {
+			author.innerHTML = "Unknown"
+		}
+	
+	});
+  }, 5000)
--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+
 // FOR RESPONSIVE NAV-BAR
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -14,6 +15,9 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get the button that submits the form
+var oyaBtn = document.querySelector(".oyaBtn") 
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
@@ -26,79 +30,87 @@ btn.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
+// TO TOGGLE PASSWORD VISIBILITY
+let Show = document.getElementById('checkBox');
+let password = document.getElementById('passy')
+
+Show.addEventListener('click' , () => {
+	let type = password.getAttribute('type') === 
+	'password' ? 'text' : 'password';
+	password.setAttribute('type' , type);
+})
 
 // ANIMATED TYPEWRITER
 
 // List of sentences
-var _CONTENT = [ 
+let _CONTENT = [ 
 	
-	'Get Noticed ...',
-	'Get Recognized ...',
-	'Get VISIBLE ...',
-	'Your DREAM JOB is a CLICK AWAY ...',
-	'Get STARTED TODAY!!!'
+	'Noticed ...',
+	'Recognized ...',
+	'VISIBLE ...',
+	'your DREAM JOB in a CLICK ...',
+	'STARTED TODAY!!!'
 ];
 
 // Current sentence being processed
-var _PART = 0;
+let part = 0;
 
 // Character number of the current sentence being processed 
-var _PART_INDEX = 0;
+let partIndex = 0;
 
 // Holds the handle returned from setInterval
-var _INTERVAL_VAL;
+let intervalVAL;
 
 // Element that holds the text
-var _ELEMENT = document.querySelector("#text");
+let element = document.querySelector("#text");
 
 // Cursor element 
-var _CURSOR = document.querySelector("#cursor");
+let cursor = document.querySelector("#cursor");
 
 // Implements typing effect
 function Type() { 
 	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
+	let text =  _CONTENT[part].substring(0, partIndex + 1);
+	element.innerHTML = text;
+	partIndex++;
 
 	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
+	if(text === _CONTENT[part]) {
 		// Hide the cursor
-		_CURSOR.style.display = 'none';
+		cursor.style.display = 'none';
 
-		clearInterval(_INTERVAL_VAL);
+		clearInterval(intervalVAL);
 		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
+			intervalVAL = setInterval(Delete, 50);
 		}, 1000);
 	}
 }
-
 // Implements deleting effect
 function Delete() {
 	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
+	let text =  _CONTENT[part].substring(0, partIndex - 1);
+	element.innerHTML = text;
+	partIndex--;
 
 	// If sentence has been deleted then start to display the next sentence
 	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+		clearInterval(intervalVAL);
 
 		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
+		if(part == (_CONTENT.length - 1))
+			part = 0;
 		else
-			_PART++;
+			part++;
 		
-		_PART_INDEX = 0;
+		partIndex = 0;
 
 		// Start to display the next sentence after some time
 		setTimeout(function() {
-			_CURSOR.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
+			cursor.style.display = 'inline-block';
+			intervalVAL = setInterval(Type, 100);
 		}, 200);
 	}
 }
 
 // Start the typing effect on load
-_INTERVAL_VAL = setInterval(Type, 200);
+intervalVAL = setInterval(Type, 200);
